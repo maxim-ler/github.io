f26c0c5122d24f7c0e222dcd75088fe4ce5f69f2
maxim-ler <maximler@mail.ru>
2019-04-10 16:36:04 +0300

    add

---
+++ test52/js/index-page.js
@@ -0,0 +1,141 @@
+$(function() {
+
+	// wow js
+	// new WOW().init();
+
+
+
+	// jQuery smooth scrolling
+	$('.go_to1').click( function() { // ловим клик по ссылке с классом go_to1
+		var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+	  
+		if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+			$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 900); // анимируем скроолинг к элементу scroll_el
+		}
+
+		return false; // выключаем стандартное действие
+	});
+
+
+
+	// accordion jquery
+	$('#accordion-js').find('h2').click(function() {
+		$(this).next().stop().slideToggle();
+	}).next().stop().hide();
+			
+
+
+	// onscroll
+	window.onscroll = function() {
+	  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+	  // console.log(scrolled);
+
+	  if(scrolled > 400) {
+	    // $('.arrow').css('display', 'block');
+	    $('.arrow').show('slow', function() {
+	      //
+	    });
+	  }
+	  else {
+	    // $('.arrow').css('display', 'none');
+	    $('.arrow').hide('slow', function() {
+	      //
+	    });
+	  }
+	}
+
+
+
+	// popup
+	// ставим на кнопку которая вызывает данный popup
+	$('.js-popup').on('click', function(e) {
+	  e.preventDefault();
+
+	  // в input hidden помещаем значение button в data-user - когда нужно передать значение из button
+	  // <button class="btnbig js-popup" data-user="узнать о программе - блок: ДИСТАНЦИОННАЯ ПРОГРАММА">узнать о программе</button>
+	  // $('.popup .main-form input[name="user_data"]').val($(this).attr('data-user'));
+
+	  // поиск popup и popup-overlay для их открытия
+	  // $('.popup').css('display', 'block');
+	  // $('.popup-overlay').css('display', 'block');
+	  // or
+	  $('.popup').slideDown();
+	  $('.popup-overlay').css('display', 'block');
+	  
+	  // поиск button close popup
+	  $('.js-close-popup, .popup-overlay').on('click', function(e) {
+			// $('.popup').css('display', 'none');
+			// $('.popup-overlay').css('display', 'none');
+			// or
+			$('.popup').slideUp('300', function() {
+				
+				// stop iframe video play after close popup
+				// var video = $(".popup iframe").attr("src");
+				// $(".popup iframe").attr("src", "");
+				// $(".popup iframe").attr("src", video);
+			});
+			$('.popup-overlay').css('display', 'none');
+			// $('.popup').find("input").val(""); // off in wp
+			
+			// для wp cf7
+			// $("form").trigger("reset");
+			// $("form .wpcf7-not-valid-tip").hide();
+			// $("form .wpcf7-response-output.wpcf7-display-none.wpcf7-validation-errors").hide();
+	  });
+	});
+
+	// $('.js-popup2').on('click', function(e) {
+	//   e.preventDefault();
+
+	//   // в input hidden помещаем значение button в data-user - когда нужно передать значение из button
+	//   // <button class="btnbig js-popup" data-user="узнать о программе - блок: ДИСТАНЦИОННАЯ ПРОГРАММА">узнать о программе</button>
+	//   // $('.popup2 .main-form input[name="user_data"]').val($(this).attr('data-user'));
+
+	//   // поиск popup и popup-overlay для их открытия
+	//   // $('.popup').css('display', 'block');
+	//   // $('.popup-overlay').css('display', 'block');
+	//   // or
+	//   $('.popup2').slideDown();
+	//   $('.popup-overlay').css('display', 'block');
+	  
+	//   // поиск button close popup
+	//   $('.js-close-popup, .popup-overlay').on('click', function(e) {
+	// 		// $('.popup').css('display', 'none');
+	// 		// $('.popup-overlay').css('display', 'none');
+	// 		// or
+	// 		$('.popup2').slideUp();
+	// 		$('.popup-overlay').css('display', 'none');
+			
+	// 		// для wp cf7
+	// 		// $("form").trigger("reset");
+	// 		// $("form .wpcf7-not-valid-tip").hide();
+	// 		// $("form .wpcf7-response-output.wpcf7-display-none.wpcf7-validation-errors").hide();
+	//   });
+	// });
+
+
+
+	// email
+	$('.main-form').submit(function(event) {
+		event.preventDefault();
+
+		$.ajax({
+			type: "POST",
+			url: "../smart.php",	// url: "smart.php", даже если js и php не в одной папке!
+			data: $(this).serialize()
+		}).done(function(data) {
+			$(this).find("input").val("");
+			
+			//alert("Заявка отправлена!");
+			window.location.href = "../thanks.html";
+			
+			$("form").trigger("reset");
+			
+			$('.popup').slideUp();
+			$('.popup-overlay').css('display', 'none');
+		});
+		
+		return false;
+	});
+
+});

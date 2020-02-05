4df72d1960c7d3eb3271f5b32f776d78d49ff6b9
vasily-d <den771981@mail.ru>
2020-02-05 16:24:49 +0300

    add

--- test70/js/index-page.js
+++ test70/js/index-page.js
@@ -4,6 +4,22 @@ $(function() {
 
 
 
+	$('.how-work-list__item img').on('click', function(e) {
+		$('.how-work-list__item img').removeClass('active');
+		$(this).addClass('active');
+
+		text = $(this).attr('data-text');
+
+
+		$('.bag-list__left h2').hide('slow');
+		$('.bag-list__left h2').show('slow', function() {
+			$('.bag-list__left h2').text(text);
+		});
+	});
+
+
+
+
 	// jQuery smooth scrolling
 	$('.go_to1').click( function() { // ловим клик по ссылке с классом go_to1
 		var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
@@ -55,12 +71,6 @@ $(function() {
 		autoplay: true,
 		autoplaySpeed: 3000,
 
-		// https://stackoverflow.com/questions/34660680/slick-slider-replace-dots-with-numbers
-		//customPaging: function(slider, i) {
-		//	var thumb = $(slider.$slides[i]).data();
-		//	return '<a class="dot">0'+i+'</a>';
-		//},
-
 		// непрерывные слайды
 		//cssEase: 'linear',
 		//autoplay: true,
@@ -110,12 +120,6 @@ $(function() {
 		autoplay: true,
 		autoplaySpeed: 3000,
 
-		// https://stackoverflow.com/questions/34660680/slick-slider-replace-dots-with-numbers
-		//customPaging: function(slider, i) {
-		//	var thumb = $(slider.$slides[i]).data();
-		//	return '<a class="dot">0'+i+'</a>';
-		//},
-
 		// непрерывные слайды
 		//cssEase: 'linear',
 		//autoplay: true,
@@ -155,21 +159,6 @@ $(function() {
 	$('.js-popup').on('click', function(e) {
 	  e.preventDefault();
 
-	  // fix popup background to scroll
-	  // $('body').addClass('no-scroll');
-	  // body.no-scroll {
-		//   overflow: hidden;
-		//   position: fixed;
-		// }
-
-	  // в input hidden помещаем значение button в data-user - когда нужно передать значение из button
-	  // <button class="btnbig js-popup" data-user="узнать о программе - блок: ДИСТАНЦИОННАЯ ПРОГРАММА">узнать о программе</button>
-	  // $('.popup .main-form input[name="user_data"]').val($(this).attr('data-user'));
-
-	  // поиск popup и popup-overlay для их открытия
-	  // $('.popup').css('display', 'block');
-	  // $('.popup-overlay').css('display', 'block');
-	  // or
 	  $('.popup').slideDown(600);
 
 	  $('.popup-overlay').fadeIn('slow');
@@ -179,27 +168,12 @@ $(function() {
 	  $('.js-close2-popup, .popup-overlay').on('click', function(e) {
 	  	e.preventDefault();
 
-	  	// upfix popup background to scroll
-	  	// $('body').removeClass('no-scroll');
-
-			// $('.popup').css('display', 'none');
-			// $('.popup-overlay').css('display', 'none');
-			// or
 			$('.popup').slideUp(600, function() {
-				// stop iframe video play after close2 popup
-				// var video = $(".popup iframe").attr("src");
-				// $(".popup iframe").attr("src", "");
-				// $(".popup iframe").attr("src", video);
+
 			});
 
 			$('.popup-overlay').fadeOut('slow');
-			// $('.popup-overlay').css('display', 'none');
-			// $('.popup').find("input").val(""); // off in wp
-
-			// для wp cf7
-			// $("form").trigger("reset");
-			// $("form .wpcf7-not-valid-tip").hide();
-			// $("form .wpcf7-response-output.wpcf7-display-none.wpcf7-validation-errors").hide();
+			
 	  });
 	});
 
@@ -253,13 +227,7 @@ $(function() {
 	$('.main-form').submit(function(event) {
 		event.preventDefault();
 
-		// проверка на стороне клиента формы через js что форма не пустая
-		// var name = $('.name1').val();
-		// var phone = $('.tel1').val();
-		// console.log(name);
-		// console.log(phone);
-
-
+		
 
 		// $.ajax({
 		// 	type: "POST",
@@ -277,49 +245,7 @@ $(function() {
 		// 	window.location.href = "thanks.html";
 		// });
 
-
-
-		// del
-		// if (name !== '' & phone !== '') {
-		// 	$.ajax({
-		// 		type: "POST",
-		// 		url: "mailer/smart.php",
-		// 		data: $(this).serialize()
-		// 	}).done(function() {
-		// 		$(this).find("input").val("");
-
-		// 		//alert("Заявка отправлена!");
-		// 		window.location.href = "../mailer/thanks.html";
-
-		// 		$("form").trigger("reset");
-
-		// 		$('.popup').slideUp();
-		// 		$('.popup-overlay').css('display', 'none');
-		// 	});
-		// } else {
-		// 	alert('Данные не заполнены!');
-		// }
-
 		return false;
 	});
-
-	// email - old
-	//$('.main-form').submit(function(event) {
-	//	event.preventDefault();
-	//	$.ajax({
-	//		type: "POST",
-	//		url: "mailer/smart.php",
-	//		data: $(this).serialize()
-	//	}).done(function() {
-	//		$(this).find("input").val("");
-	//		alert("Сообщение успешно отправлено");
-	//		$("form").trigger("reset");
-	//
-	//		$('.popup').slideUp();
-	//		$('.popup-overlay').css('display', 'none');
-	//	});
-	//	return false;
-	//});
-
 
 });
